ff8090c6d9fb1dc04b668e49b9cc3ce74052dd40
Jerit-Jose <jeritjose17@gmail.com>
2024-05-08 10:24:17 +0530

    update

--- Nodes/Node7_Motion_Detector/Node7_Motion_Detector.ino
+++ Nodes/Node7_Motion_Detector/Node7_Motion_Detector.ino
@@ -2,82 +2,71 @@
 #include <ESP8266WiFi.h>
 #define THINGSBOARD_ENABLE_PROGMEM 0
 #endif  // ESP8266
-
 #include <ThingsBoard.h>
 #include <Servo.h>
-
-// constexpr char WIFI_SSID[] = "NITK-NET";
-// constexpr char WIFI_PASSWORD[] = "2K16NITK";
-
-constexpr char WIFI_SSID[] = "CSD";
-constexpr char WIFI_PASSWORD[] = "csd@NITK2014";
-
-constexpr char TOKEN[] = "TMMdzP4UaqyZw4I3suXP";
-
-constexpr char THINGSBOARD_SERVER[] = "10.100.80.26";
-constexpr uint16_t THINGSBOARD_PORT = 1883U;
-constexpr uint32_t MAX_MESSAGE_SIZE = 128U;
-constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
-
-IPAddress localIP(10, 100, 80, 41);
-IPAddress gateway(10, 100, 80, 1);
-IPAddress subnet(255, 255, 252, 0);
-
-IPAddress dns1(10, 20, 1, 22);
-IPAddress dns2(10, 3, 0, 101);
-
-constexpr char MOTION_KEY[] = "motion";
-constexpr char ACTUATOR_KEY[] = "actuator";
-
-constexpr const char RPC_TEMPERATURE_METHOD[] = "example_set_temperature";
-constexpr const char RPC_SWITCH_METHOD[] = "example_set_switch";
-constexpr const char RPC_TEMPERATURE_KEY[] = "temp";
-constexpr const char RPC_SWITCH_KEY[] = "switch";
-constexpr const char RPC_RESPONSE_KEY[] = "example_response";
 
 #define LED_PIN 12
 #define PIR_PIN 14
 #define SERVO_PIN 13
-
 #define WIFI_STATUS_LED LED_BUILTIN
+#define CLOSE_POS 180
+#define OPEN_POS 40
+#define DEBOUNCE_TIME 6000
 
-WiFiClient espClient;
-ThingsBoard tb(espClient, MAX_MESSAGE_SIZE);
+constexpr char WIFI_SSID[] = "CSD";
+constexpr char WIFI_PASSWORD[] = "csd@NITK2014";
+constexpr char TOKEN[] = "Ct0199UNxnMBtfUeSpLE";
+constexpr char THINGSBOARD_SERVER[] = "10.14.0.205";
+constexpr uint16_t THINGSBOARD_PORT = 1883U;
+constexpr uint32_t MAX_MESSAGE_SIZE = 128U;
+constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
 
-Servo myservo;
-
+constexpr char MOTION_KEY[] = "motion";
+constexpr char ACTUATOR_KEY[] = "actuator";
+constexpr const char MOTION_TELEMETRY[] = "telemetry_data";
+constexpr const char RPC_SWITCH_METHOD[] = "openDoor";
+constexpr const char RPC_TEMPERATURE_KEY[] = "temp";
+constexpr const char RPC_SWITCH_KEY[] = "openDoor";
+constexpr const char RPC_RESPONSE_KEY[] = "example_response";
 int status = WL_IDLE_STATUS;  // the Wifi radio's status
 bool subscribed = false;
-
 //variables to keep track of the timing of recent interrupts
 unsigned long button_time = 0;
 unsigned long last_button_time = 0;
-
-#define CLOSE_POS 180
-#define OPEN_POS 40
-
-#define DEBOUNCE_TIME 6000
-
 bool motionDetected = false;
-
 int pos = 0;
-
 int actuatorState = 0;
+
+int switch_state;
+
+unsigned long switchTimer = 0;
+const int switchDuration = 7000;  // 5 seconds in milliseconds
+
+unsigned long previousMillis = 0;  // will store last time LED was updated
+
+// constants won't change:
+const long interval = 2000;  // interval at which to blink (milliseconds)
+
+Servo myservo;  //object  created for servo
+
+WiFiClient espClient;
+ThingsBoard tb(espClient, MAX_MESSAGE_SIZE);
 
 /// @brief Initalizes WiFi connection,
 // will endlessly delay until a connection has been successfully established
 void InitWiFi() {
 
-  // Configure static IP and DNS server
-  WiFi.config(localIP, gateway, subnet, dns1, dns2);
-  // WiFi.setDNS(dns1, dns2);
+  Serial.print("Attempting to connect to network: ");
+  Serial.println(WIFI_SSID);
 
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
-    Serial.println(WIFI_SSID);
-    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    delay(5000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
   }
+
   Serial.println("Connected to AP");
 }
 
@@ -89,7 +78,6 @@ bool reconnect() {
   if (status == WL_CONNECTED) {
     return true;
   }
-
   // If we aren't establish a new connection to the given WiFi network
   InitWiFi();
   return true;
@@ -108,7 +96,6 @@ RPC_Response processTemperatureChange(const RPC_Data &data) {
 
   Serial.print("Example temperature: ");
   Serial.println(example_temperature);
-
   // Just an response example
   StaticJsonDocument<JSON_OBJECT_SIZE(1)> doc;
   doc[RPC_RESPONSE_KEY] = 42;
@@ -120,24 +107,17 @@ RPC_Response processTemperatureChange(const RPC_Data &data) {
 /// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
 /// "data" Data containing the rpc data that was called and its current value
 /// Response that should be sent to the cloud. Useful for getMethods
-RPC_Response processSwitchChange(const RPC_Data &data) {
-  Serial.println("Received the set switch method");
-
-  // Process data
-  const bool switch_state = data[RPC_SWITCH_KEY];
-
-  Serial.print("Example switch state: ");
-  Serial.println(switch_state);
-
-  // Just an response example
-  StaticJsonDocument<JSON_OBJECT_SIZE(1)> doc;
-  doc[RPC_RESPONSE_KEY] = 22.02;
-  return RPC_Response(doc);
+RPC_Response setServoSwitchState(RPC_Data &data) {
+  Serial.println("RECIEVED SWITCH STATE");
+  switch_state = data;
+  Serial.println("SWITCH STATE CHANGE:");
+  Serial.print(switch_state);
+  return RPC_Response("setServoSwitchValue", switch_state);
 }
 
 const std::array<RPC_Callback, 2U> callbacks = {
-  RPC_Callback{ RPC_TEMPERATURE_METHOD, processTemperatureChange },
-  RPC_Callback{ RPC_SWITCH_METHOD, processSwitchChange }
+  RPC_Callback{ "setServoSwitchValue", setServoSwitchState },
+  RPC_Callback{ RPC_SWITCH_METHOD, setServoSwitchState }
 };
 
 // Checks if motion was detected
@@ -150,16 +130,20 @@ ICACHE_RAM_ATTR void detectMotion() {
 }
 
 void openDoor() {
-  for (pos = CLOSE_POS; pos >= OPEN_POS; pos -= 10) {
-    myservo.write(pos);
-    delay(1);
+  if (myservo.read() != OPEN_POS) {
+    for (pos = CLOSE_POS; pos >= OPEN_POS; pos -= 10) {
+      myservo.write(pos);
+      delay(1);
+    }
   }
 }
 
 void closeDoor() {
-  for (pos = OPEN_POS; pos <= CLOSE_POS; pos += 10) {
-    myservo.write(pos);
-    delay(1);
+  if (myservo.read() != CLOSE_POS) {
+    for (pos = OPEN_POS; pos <= CLOSE_POS; pos += 10) {
+      myservo.write(pos);
+      delay(1);
+    }
   }
 }
 
@@ -185,6 +169,8 @@ void setup() {
 }
 
 void loop() {
+
+  unsigned long currentMillis = millis();
   delay(1000);
 
   if (!reconnect()) {
@@ -205,7 +191,6 @@ void loop() {
 
   if (!subscribed) {
     Serial.println("Subscribing for RPC...");
-
     // Perform a subscription. All consequent data processing will happen in
     // processTemperatureChange() and processSwitchChange() functions,
     // as denoted by callbacks array.
@@ -219,27 +204,39 @@ void loop() {
     subscribed = true;
   }
 
-  if (motionDetected) {
-    Serial.println("Motion Detected!");
-    digitalWrite(LED_PIN, HIGH);
-    actuatorState = 1;
-    tb.sendAttributeBool(MOTION_KEY, motionDetected);
-    tb.sendTelemetryString(ACTUATOR_KEY, "Door is Open");
-    openDoor();
-    delay(3000);
-    closeDoor();
-    motionDetected = false;
+  tb.sendTelemetryBool(MOTION_KEY, motionDetected);
+
+  Serial.println();
+  Serial.print(motionDetected);
+  Serial.print("\t");
+  Serial.println(digitalRead(PIR_PIN));
+
+  // Check if switch_state is 40
+  if (switch_state == 40) {
+    // Check if the switch timer is not running
+    if (switchTimer == 0) {
+      digitalWrite(LED_PIN, HIGH);  // Turn on the LED
+      tb.sendTelemetryString(MOTION_TELEMETRY, "Door is open");
+      tb.sendAttributeBool(ACTUATOR_KEY, true);
+      openDoor();  // Open the door
+      // Start the timer
+      switchTimer = millis();  // Record the current time
+    }
   } else {
-    digitalWrite(LED_PIN, LOW);
-    myservo.write(CLOSE_POS);
-    actuatorState = 0;
-    tb.sendAttributeBool(MOTION_KEY, motionDetected);
-    tb.sendTelemetryString(ACTUATOR_KEY, "Door is Close");
+    digitalWrite(LED_PIN, LOW);  // Turn off the LED
+    tb.sendTelemetryString(MOTION_TELEMETRY, "Door is closed");
+    tb.sendAttributeBool(ACTUATOR_KEY, false);
+    closeDoor();  // Close the door
   }
 
-  // Uploads new telemetry to ThingsBoard using MQTT.
-  Serial.println("Sending motion detection data...");
+  // Reset motionDetected flag
+  motionDetected = false;
 
-
+  // Check if 5 seconds have passed since the switch was activated
+  if (switchTimer != 0 && millis() - switchTimer >= switchDuration) {
+    // Reset switch state and timer
+    switch_state = 0;  // Reset switch state
+    switchTimer = 0;   // Reset the switch timer
+  }
   tb.loop();
 }

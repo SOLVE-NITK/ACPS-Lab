562c981525dc80817174e8889aa8027fd5aaf82e
Jerit-Jose <jeritjose17@gmail.com>
2024-06-10 16:01:29 +0530

    updated

--- Nodes/Node10_Temperature_Control_System/Node10_Temperature_Control_System.ino
+++ Nodes/Node10_Temperature_Control_System/Node10_Temperature_Control_System.ino
@@ -3,12 +3,13 @@
 #include <LM35.h>
 #include <WiFi.h>
 
+#include <ArduinoOTA.h>
+#include <ArduinoHttpClient.h>
+
 #define LM35_PIN A1  // Analog pin A0 for LM35
 #define LED_PIN 7    // Digital pin for LED
 #define FAN_PIN 8    // Digital pin for Fan
-
 #define TEMP_THRESHOLD 43
-
 #define WIFI_STATUS_LED LED_BUILTIN
 
 // constexpr char WIFI_SSID[] = "NITK-NET";
@@ -21,30 +22,56 @@ constexpr char TOKEN[] = "WozbMrsNP48z2dEtAn29";
 
 constexpr char THINGSBOARD_SERVER[] = "10.14.0.205";
 constexpr uint16_t THINGSBOARD_PORT = 1883U;
-constexpr uint32_t MAX_MESSAGE_SIZE = 128U;
+constexpr uint32_t MAX_MESSAGE_SIZE = 256U;
 constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
 
-constexpr char TEMPERATURE_KEY[] = "temperature";
+constexpr char TEMPERATURE_KEY[] = "Temperature";
 constexpr char ACTUATOR_KEY[] = "actuatorState";
 
 constexpr const char TEMPERATURE_TELEMETRY[] = "temperature_data";
-constexpr const char RPC_SWITCH_METHOD[] = "temp";
-constexpr const char RPC_TEMPERATURE_KEY[] = "temp";
-constexpr const char RPC_SWITCH_KEY[] PROGMEM = "switch";
-constexpr const char RPC_RESPONSE_KEY[] = "example_response";
-
-WiFiClient espClient;
-ThingsBoard tb(espClient, MAX_MESSAGE_SIZE);
-
-LM35 sensor(LM35_PIN);
+constexpr const char RPC_TEMP_METHOD[] = "temp";
 
 int status = WL_IDLE_STATUS;  // the Wifi radio's status
 bool subscribed = false;
+int temp_value;
 
-unsigned long startTime;
-bool runningCode = false;
+bool requestedShared = false;
+int msg = 0;
 
-int switch_state;
+char *BASE_URL = "/api/v1";   // Define base URL for API requests
+char *ENDPOINT = "firmware";  // Define endpoint for firmware updates
+char PATH[256];               // Define array to store the path for firmware updates
+
+constexpr const char TEST_KEY[] PROGMEM = "test";
+constexpr const char FW_TAG_KEY[] PROGMEM = "target_fw_ts";
+constexpr const char FW_CHKS_KEY[] PROGMEM = "fw_checksum";
+constexpr const char FW_CHKS_ALGO_KEY[] PROGMEM = "fw_checksum_algorithm";
+constexpr const char FW_SIZE_KEY[] PROGMEM = "fw_tag";
+constexpr const char FW_TITLE_KEY[] PROGMEM = "fw_title";
+constexpr const char FW_VER_KEY[] PROGMEM = "fw_version";
+
+char CURRENT_VERSION[] = "1.0.0";
+constexpr int FIRMWARE_SIZE = 20;           // Adjust the size according to your requirements
+char NEW_VERSION[FIRMWARE_SIZE] = "1.0.0";  // Declare NEW_VERSION array
+
+char FW_TITLE[] = "NANOTC";
+constexpr int TITLE_SIZE = 20;          // Adjust the size according to your requirements
+char FWW_TITLE[TITLE_SIZE] = "NANOTC";  // Declare NEW_VERSION array
+
+// Shared attributes we want to request from the server
+constexpr std::array<const char *, 6U> REQUESTED_SHARED_ATTRIBUTES = {
+  FW_CHKS_KEY,
+  FW_CHKS_ALGO_KEY,
+  FW_SIZE_KEY,
+  FW_TAG_KEY,
+  FW_TITLE_KEY,
+  FW_VER_KEY
+};
+
+WiFiClient wifiClient;
+ThingsBoard tb(wifiClient, MAX_MESSAGE_SIZE);
+
+LM35 sensor(LM35_PIN);
 
 /// @brief Initalizes WiFi connection,
 // will endlessly delay until a connection has been successfully established
@@ -79,41 +106,110 @@ bool reconnect() {
   return true;
 }
 
-/// Processes function for RPC call "example_set_temperature"
-/// RPC_Data is a JSON variant, that can be queried using operator[]
-/// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
-/// "data" Data containing the rpc data that was called and its current value
-///  Response that should be sent to the cloud. Useful for getMethods
-RPC_Response processTemperatureChange(const RPC_Data &data) {
-  Serial.println("Received the set temperature RPC method");
+void processSharedAttributeRequest(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    Serial.println(it->key().c_str());
+    // Shared attributes have to be parsed by their type.
+    Serial.println(it->value().as<const char *>());
+    if (strcmp_P(it->key().c_str(), FW_VER_KEY) == 0) {
+      // If the key is "CURRENT_VERSION", print its value
+      Serial.print("NEW_VERSION: ");
+      // Copy the value to NEW_VERSION array
+      strncpy(NEW_VERSION, it->value().as<const char *>(), FIRMWARE_SIZE - 1);
+      // Ensure null termination
+      NEW_VERSION[FIRMWARE_SIZE - 1] = '\0';
+      // Print the value
+      Serial.println(NEW_VERSION);
+    }
+    if (strcmp_P(it->key().c_str(), FW_TITLE_KEY) == 0) {
+      // If the key is "CURRENT_VERSION", print its value
+      Serial.print("FWW_TITLE: ");
+      // Copy the value to NEW_VERSION array
+      strncpy(FWW_TITLE, it->value().as<const char *>(), TITLE_SIZE - 1);
+      // Ensure null termination
+      FWW_TITLE[TITLE_SIZE - 1] = '\0';
+      // Print the value
+      Serial.println(FWW_TITLE);
+    }
+  }
 
-  // Process data
-  const float example_temperature = data[RPC_TEMPERATURE_KEY];
-
-  Serial.print("Example temperature: ");
-  Serial.println(example_temperature);
-
-  // Just an response example
-  StaticJsonDocument<JSON_OBJECT_SIZE(1)> doc;
-  doc[RPC_RESPONSE_KEY] = 42;
-  return RPC_Response(doc);
+  int jsonSize = JSON_STRING_SIZE(measureJson(data));
+  char buffer[jsonSize];
+  serializeJson(data, buffer, jsonSize);
+  Serial.println(buffer);
 }
 
-/// Processes function for RPC call "example_set_switch"
-/// RPC_Data is a JSON variant, that can be queried using operator[]
-/// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
-/// "data" Data containing the rpc data that was called and its current value
-/// Response that should be sent to the cloud. Useful for getMethods
-RPC_Response setServoSwitchState(RPC_Data &data) {
-  Serial.println("RECIEVED SWITCH STATE");
-  switch_state = data;
-  Serial.println("SWITCH STATE CHANGE:");
-  Serial.print(switch_state);
-  return RPC_Response("actuatorState", switch_state);
+const Attribute_Request_Callback sharedCallback(REQUESTED_SHARED_ATTRIBUTES.cbegin(), REQUESTED_SHARED_ATTRIBUTES.cend(), &processSharedAttributeRequest);
+
+void handleSketchDownload(const char *token, char *title, char *CURRENT_VERSION) {
+  sprintf(PATH, "%s/%s/%s?title=%s&version=%s", BASE_URL, token, ENDPOINT, title, CURRENT_VERSION);
+  // const char* THINGSBOARD_SERVER = "10.100.80.25";  // Set your correct hostname
+  const unsigned short SERVER_PORT = 8090U;                        // Commonly 80 (HTTP) | 443 (HTTPS)
+  HttpClient client(wifiClient, THINGSBOARD_SERVER, SERVER_PORT);  // HTTP
+  // HttpClient client(wifiClientSSL, SERVER, SERVER_PORT);  // HTTPS
+  char buff[66];                           //enough size should be given to the buffer otherwise it won't get updated
+  snprintf(buff, sizeof(buff), PATH);      // Copy the URL path to the buffer
+  Serial.print("Check for update file ");  // Print message to indicate checking for update file with the URL path
+  Serial.println(buff);
+  // Make the GET request
+  client.get(buff);                              // Make a GET request to the server with the URL path
+  int statusCode = client.responseStatusCode();  // Retrieve the HTTP status code from the response
+  Serial.print("Update status code: ");          // Print the update status code received from the server
+  Serial.println(statusCode);
+
+  if (statusCode != 200) {  // Check if the status code indicates a successful response (HTTP 200 OK)
+    client.stop();          // If the status code is not 200, stop the client connection and return
+    return;
+  }
+
+  long length = client.contentLength();  // Retrieve the content length of the response from the server
+
+  if (length == HttpClient::kNoContentLengthHeader) {                                            // Check if the server provided a Content-Length header
+    client.stop();                                                                               // Stop the client connection
+    Serial.println("Server didn't provide Content-length header. Can't continue with update.");  // Print error message
+    return;                                                                                      // Exit the function as the update cannot proceed without content length information
+  }
+  Serial.print("Server returned update file of size ");  // Print message indicating successful response from server
+  Serial.print(length);                                  // Print the size of the update file in bytes
+  Serial.println(" bytes");                              // Print bytes
+
+  if (!InternalStorage.open(length)) {                                                             // Attempt to open InternalStorage to store the update
+    client.stop();                                                                                 // Stop the client connection
+    Serial.println("There is not enough space to store the update. Can't continue with update.");  // Print error message
+    return;                                                                                        // Exit the function as there is insufficient space for the update
+  }
+  byte b;                          // Declare a variable 'b' of type byte to store data read from the client
+  while (length > 0) {             // Start a loop to read bytes from the client until 'length' becomes zero
+    if (!client.readBytes(&b, 1))  // reading a byte with timeout
+      break;                       // If unable to read a byte within the timeout, exit the loop
+    InternalStorage.write(b);      // Write the read byte to InternalStorage
+    length--;                      // Decrement the length to keep track of bytes read
+  }
+  InternalStorage.close();  // Close the InternalStorage after writing all bytes
+
+  client.stop();                                            // Stop the client connection after the update process
+  if (length > 0) {                                         // Check if there are remaining bytes to be read
+    Serial.print("Timeout downloading update file at ");    // Print error message for timeout
+    Serial.print(length);                                   // Print the number of remaining bytes
+    Serial.println(" bytes. Can't continue with update.");  // Print error message
+    return;                                                 // Exit the function if unable to download the complete update
+  }
+
+  Serial.println("Sketch update apply and reset.");  // Print message indicating successful update
+  Serial.flush();                                    // Flush the serial buffer to ensure all data is sent
+  InternalStorage.apply();                           // this doesn't return
+}
+
+RPC_Response setTemeratureState(RPC_Data &data) {
+  Serial.println("RECIEVED VALUE");
+  temp_value = data;
+  // Serial.println("VALUE:");
+  // Serial.print(temp_value);
+  return RPC_Response("actuatorState", 0);
 }
 
 const std::array<RPC_Callback, 1U> callbacks = {
-  RPC_Callback{ RPC_SWITCH_METHOD, setServoSwitchState }
+  RPC_Callback{ RPC_TEMP_METHOD, setTemeratureState }
 };
 
 void setup() {
@@ -124,14 +220,11 @@ void setup() {
   pinMode(LED_PIN, OUTPUT);  // Set LED pin as output
   pinMode(FAN_PIN, OUTPUT);  // Set fan pin as output
   pinMode(WIFI_STATUS_LED, OUTPUT);
-
-  InitWiFi();
-
   digitalWrite(LED_PIN, LOW);  // Turn off the LED
   digitalWrite(FAN_PIN, LOW);  // Turn off the Fan
   digitalWrite(WIFI_STATUS_LED, LOW);
 
-  // actuatorState = 0;
+  InitWiFi();
 }
 
 void loop() {
@@ -173,33 +266,45 @@ void loop() {
     subscribed = true;
   }
 
+  if (!requestedShared) {
+    Serial.println("Requesting shared attributes...");
+    requestedShared = tb.Shared_Attributes_Request(sharedCallback);
+    if (!requestedShared) {
+      Serial.println("Failed to request shared attributes");
+    }
+  }
+
+  if (strcmp(FWW_TITLE, FW_TITLE) == 0) {
+    if (strcmp(NEW_VERSION, CURRENT_VERSION) != 0) {
+      // Perform actions if FW_version matches the desired version
+      strcpy(CURRENT_VERSION, NEW_VERSION);
+      Serial.println("\n");
+      Serial.println("new FW_version available.");
+      Serial.println("\n");
+      handleSketchDownload(TOKEN, FW_TITLE, CURRENT_VERSION);
+
+    } else {
+      if (msg == 0) {
+        Serial.println("\n");
+        Serial.println("updates NOT available...");
+        Serial.println("\n");
+        msg = 1;
+      }
+    }
+  }
+
   int temperature = sensor.getTemp();  // Read temperature from LM35 sensor
   Serial.print("Temperature: ");
   Serial.println(temperature);
 
+  tb.sendTelemetryInt(TEMPERATURE_KEY, temperature);
 
-  // Check if switch_state is 30
-  if (switch_state == 30) {
-    switch_state = 0;
-    startTime = millis();  // Record the start time
-    runningCode = true;    // Set flag to indicate code is running
-  }
-
-  // Run the code for 4 seconds if the flag is set
-  if (runningCode) {
-    // Run your code here
+  if (temp_value == 30) {
+    temp_value = 0;
     digitalWrite(LED_PIN, HIGH);  // Turn on the LED
     digitalWrite(FAN_PIN, LOW);   // Turn on the Fan
     tb.sendTelemetryString(TEMPERATURE_TELEMETRY, "Temperature Upnormal");
     tb.sendAttributeBool(ACTUATOR_KEY, true);
-
-    // Check if 4 seconds have elapsed
-    if (millis() - startTime >= 4000) {
-      // Reset the flag and turn off the LED and Fan after 4 seconds
-      runningCode = false;
-      digitalWrite(LED_PIN, LOW);  // Turn off the LED
-      digitalWrite(FAN_PIN, LOW);  // Turn off the Fan
-    }
   } else {
     // Code for the else block
     tb.sendTelemetryString(TEMPERATURE_TELEMETRY, "Temperature Normal");
@@ -207,11 +312,6 @@ void loop() {
     digitalWrite(LED_PIN, LOW);  // Turn off the LED
     digitalWrite(FAN_PIN, LOW);  // Turn off the Fan
   }
-
-  // Uploads new telemetry to ThingsBoard using MQTT.
-  // Serial.println("Sending data...");
-  tb.sendTelemetryInt(TEMPERATURE_KEY, temperature);
-  // tb.sendAttributeBool(ACTUATOR_KEY, actuatorState);
 
   tb.loop();
 }

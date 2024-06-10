1ce27a38a07a30a589d9a6afa798f8ffd3a58323
Jerit-Jose <jeritjose17@gmail.com>
2024-06-10 10:37:48 +0530

    updated

--- Nodes/Node4_Fire_Smoke_Detector/Node4_Fire_Smoke_Detector.ino
+++ Nodes/Node4_Fire_Smoke_Detector/Node4_Fire_Smoke_Detector.ino
@@ -1,56 +1,9 @@
 #include <ThingsBoard.h>
 #include <WiFiNINA.h>
+#include <WiFi.h>
+
 #include <ArduinoOTA.h>
 #include <ArduinoHttpClient.h>
-
-constexpr char WIFI_SSID[] = "CSD";
-constexpr char WIFI_PASSWORD[] = "csd@NITK2014";
-
-constexpr char TOKEN[] = "8ZUAtcOeTtOzaviknlfT";
-
-constexpr char THINGSBOARD_SERVER[] = "10.14.0.205";
-constexpr uint16_t THINGSBOARD_PORT = 1883U;
-constexpr uint32_t MAX_MESSAGE_SIZE = 128U;
-constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
-
-constexpr char FIRE_KEY[] = "fire";
-constexpr char SMOKE_KEY[] = "smoke";
-constexpr char ACTUATOR_KEY[] PROGMEM = "actuator";
-
-constexpr const char RPC_FIRE_METHOD[] = "example_set_fire";
-constexpr const char RPC_SWITCH_METHOD[] = "example_set_switch";
-constexpr const char RPC_FIRE_KEY[] = "temp";
-constexpr const char RPC_SWITCH_KEY[] PROGMEM = "switch";
-constexpr const char RPC_RESPONSE_KEY[] = "example_response";
-
-WiFiClient espClient;
-ThingsBoard tb(espClient, MAX_MESSAGE_SIZE);
-
-uint8_t status = WL_IDLE_STATUS;  // the Wifi radio's status
-bool subscribed = false;
-bool requestedShared = false;
-int msg = 0;
-
-char *BASE_URL = "/api/v1";   // Define base URL for API requests
-char *ENDPOINT = "firmware";  // Define endpoint for firmware updates
-char PATH[256];               // Define array to store the path for firmware updates
-
-constexpr const char FW_TITLE_KEY[] = "fw_title";
-constexpr const char FW_VER_KEY[] = "fw_version";
-
-char CURRENT_VERSION[] = "1.0.0";
-constexpr int FIRMWARE_SIZE = 20;           // Adjust the size according to your requirements
-char NEW_VERSION[FIRMWARE_SIZE] = "1.0.0";  // Declare NEW_VERSION array
-
-char FW_TITLE[] = "RPi";
-constexpr int TITLE_SIZE = 20;       // Adjust the size according to your requirements
-char FWW_TITLE[TITLE_SIZE] = "RPi";  // Declare NEW_VERSION array
-
-// Shared attributes we want to request from the server
-constexpr std::array<const char *, 2U> REQUESTED_SHARED_ATTRIBUTES = {
-  FW_TITLE_KEY,
-  FW_VER_KEY
-};
 
 // Pin definitions
 #define IR_PIN 2
@@ -63,27 +16,99 @@ constexpr std::array<const char *, 2U> REQUESTED_SHARED_ATTRIBUTES = {
 // Threshold values
 #define SMOKE_THRESHOLD 200
 
+#define DEBOUNCE_TIME 3000
+#define BLINK_INTERVAL 250
+
+constexpr char WIFI_SSID[] = "CSD";
+constexpr char WIFI_PASSWORD[] = "csd@NITK2014";
+
+constexpr char TOKEN[] = "3bKA7viQgnH8IXTWL2TT";
+
+constexpr char THINGSBOARD_SERVER[] = "10.14.0.205";
+constexpr uint16_t THINGSBOARD_PORT = 1883U;
+constexpr uint32_t MAX_MESSAGE_SIZE = 256U;
+constexpr uint32_t SERIAL_DEBUG_BAUD = 115200U;
+
+constexpr char FIRE_KEY[] = "fire";
+constexpr char SMOKE_KEY[] = "smoke";
+constexpr char ACTUATOR_KEY[] PROGMEM = "actuator";
+
+constexpr const char RPC_FIRE_SENSOR_DATA[] = "irSensorValue";
+constexpr const char RPC_SMOKE_SENSOR_DATA[] = "mq11SensorValue";
+constexpr const char RPC_FIRE_METHOD[] = "fire";
+constexpr const char RPC_SMOKE_METHOD[] = "smoke";
+
+int status = WL_IDLE_STATUS;  // the Wifi radio's status
+bool subscribed = false;
 int buzzerVolume = 5;
-
 int actuatorState;
+int FireState = 0, SmokeState = 0;
+bool buzzerState = false;
 
+bool requestedShared = false;
+int msg = 0;
+
+char *BASE_URL = "/api/v1";   // Define base URL for API requests
+char *ENDPOINT = "firmware";  // Define endpoint for firmware updates
+char PATH[256];               // Define array to store the path for firmware updates
+
+constexpr const char TEST_KEY[] PROGMEM = "test";
+constexpr const char FW_TAG_KEY[] PROGMEM = "target_fw_ts";
+constexpr const char FW_CHKS_KEY[] PROGMEM = "fw_checksum";
+constexpr const char FW_CHKS_ALGO_KEY[] PROGMEM = "fw_checksum_algorithm";
+constexpr const char FW_SIZE_KEY[] PROGMEM = "fw_tag";
+constexpr const char FW_TITLE_KEY[] PROGMEM = "fw_title";
+constexpr const char FW_VER_KEY[] PROGMEM = "fw_version";
+
+char CURRENT_VERSION[] = "1.0.3";
+constexpr int FIRMWARE_SIZE = 20;           // Adjust the size according to your requirements
+char NEW_VERSION[FIRMWARE_SIZE] = "1.0.3";  // Declare NEW_VERSION array
+
+char FW_TITLE[] = "MKRFS";
+constexpr int TITLE_SIZE = 20;          // Adjust the size according to your requirements
+char FWW_TITLE[TITLE_SIZE] = "MKRFS";  // Declare NEW_VERSION array
+
+// Shared attributes we want to request from the server
+constexpr std::array<const char *, 6U> REQUESTED_SHARED_ATTRIBUTES = {
+  FW_CHKS_KEY,
+  FW_CHKS_ALGO_KEY,
+  FW_SIZE_KEY,
+  FW_TAG_KEY,
+  FW_TITLE_KEY,
+  FW_VER_KEY
+};
+
+WiFiClient wifiClient;
+ThingsBoard tb(wifiClient, MAX_MESSAGE_SIZE);
+
+/// @brief Initalizes WiFi connection,
+// will endlessly delay until a connection has been successfully established
 void InitWiFi() {
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
+  Serial.begin(115200);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
     Serial.println(WIFI_SSID);
-    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    delay(5000);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    delay(5000);  // Wait for 5 seconds before checking connection status
+    if (attempts >= 10) {
+      Serial.println("Connection failed after 10 attempts.");
+      return;  // Exit the function if connection fails after 10 attempts
+    }
+    attempts++;
   }
-  Serial.println("Connected to AP");
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
-
+/// @brief Reconnects the WiFi uses InitWiFi if the connection has been removed
+/// @return Returns true as soon as a connection has been established again
 bool reconnect() {
   // Check to ensure we aren't connected yet
-  status = WiFi.status();
+  const uint8_t status = WiFi.status();
   if (status == WL_CONNECTED) {
     return true;
   }
-
   // If we aren't establish a new connection to the given WiFi network
   InitWiFi();
   return true;
@@ -124,13 +149,12 @@ void processSharedAttributeRequest(const Shared_Attribute_Data &data) {
 
 const Attribute_Request_Callback sharedCallback(REQUESTED_SHARED_ATTRIBUTES.cbegin(), REQUESTED_SHARED_ATTRIBUTES.cend(), &processSharedAttributeRequest);
 
-void handleSketchDownload(const char *token, char *title, char *CURRENT_VERSION) {
-  sprintf(PATH, "%s/%s/%s?title=%s&version=%s", BASE_URL, token, ENDPOINT, title, CURRENT_VERSION);
-  // const char* THINGSBOARD_SERVER = "10.100.80.25";  // Set your correct hostname
-  const unsigned short SERVER_PORT = 8090U;                       // Commonly 80 (HTTP) | 443 (HTTPS)
-  HttpClient client(espClient, THINGSBOARD_SERVER, SERVER_PORT);  // HTTP
+void handleSketchDownload() {
+  sprintf(PATH, "%s/%s/%s?title=%s&version=%s", BASE_URL, TOKEN, ENDPOINT, FW_TITLE, CURRENT_VERSION);
+  const unsigned short SERVER_PORT = 8090U;                        // Commonly 80 (HTTP) | 443 (HTTPS)
+  HttpClient client(wifiClient, THINGSBOARD_SERVER, SERVER_PORT);  // HTTP
   // HttpClient client(wifiClientSSL, SERVER, SERVER_PORT);  // HTTPS
-  char buff[64];
+  char buff[66];                           //enough size should be given to the buffer otherwise it won't get updated
   snprintf(buff, sizeof(buff), PATH);      // Copy the URL path to the buffer
   Serial.print("Check for update file ");  // Print message to indicate checking for update file with the URL path
   Serial.println(buff);
@@ -183,52 +207,50 @@ void handleSketchDownload(const char *token, char *title, char *CURRENT_VERSION)
   InternalStorage.apply();                           // this doesn't return
 }
 
-RPC_Response processfireChange(const RPC_Data &data) {
-  Serial.println("Received the set fire RPC method");
-
-  // Process data
-  const float example_fire = data[RPC_FIRE_KEY];
-
-  Serial.print("Example fire: ");
-  Serial.println(example_fire);
-
-  // Just an response example
-  StaticJsonDocument<JSON_OBJECT_SIZE(1)> doc;
-  doc[RPC_RESPONSE_KEY] = 42;
-  return RPC_Response(doc);
+RPC_Response processSmokeState(RPC_Data &data) {
+  Serial.println("RECIEVED SMOKE STATE");
+  SmokeState = data;
+  Serial.println("SMOKE STATE CHANGE:");
+  Serial.print(SmokeState);
+  return RPC_Response("actuatorState", SmokeState);
 }
 
-RPC_Response processSwitchChange(const RPC_Data &data) {
-  Serial.println("Received the set switch method");
-
-  // Process data
-  const bool switch_state = data[RPC_SWITCH_KEY];
-
-  Serial.print("Example switch state: ");
-  Serial.println(switch_state);
-
-  // Just an response example
-  StaticJsonDocument<JSON_OBJECT_SIZE(1)> doc;
-  doc[RPC_RESPONSE_KEY] = 22.02;
-  return RPC_Response(doc);
+RPC_Response processFireState(RPC_Data &data) {
+  Serial.println("RECIEVED FIRE STATE");
+  FireState = data;
+  Serial.println("FIRE STATE CHANGE:");
+  Serial.print(FireState);
+  return RPC_Response("actuatorState", FireState);
 }
 
 const std::array<RPC_Callback, 2U> callbacks = {
-  RPC_Callback{ RPC_FIRE_METHOD, processfireChange },
-  RPC_Callback{ RPC_SWITCH_METHOD, processSwitchChange }
+  RPC_Callback{ RPC_SMOKE_METHOD, processSmokeState },
+  RPC_Callback{ RPC_FIRE_METHOD, processFireState }
 };
 
 
 void activateAlarm() {
-  analogWrite(BUZZER_PIN, buzzerVolume);
-  digitalWrite(LED_PIN, HIGH);
-  actuatorState = 1;
-}
+  unsigned long startMillis = millis();
+  unsigned long previousMillis = millis();
 
-void deactivateAlarm() {
-  analogWrite(BUZZER_PIN, 0);
+  unsigned long interval = BLINK_INTERVAL;
+  unsigned long duration = DEBOUNCE_TIME;
+
+  while (previousMillis - startMillis < duration) {
+    if (millis() - previousMillis >= interval) {
+      previousMillis = millis();
+      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+      if (buzzerState == false) {
+        analogWrite(BUZZER_PIN, buzzerVolume);
+        buzzerState = true;
+      } else {
+        analogWrite(BUZZER_PIN, 0);
+        buzzerState = false;
+      }
+    }
+  }
   digitalWrite(LED_PIN, LOW);
-  actuatorState = 0;
+  analogWrite(BUZZER_PIN, 0);
 }
 
 void setup() {
@@ -299,7 +321,7 @@ void loop() {
       Serial.println("\n");
       Serial.println("new FW_version available.");
       Serial.println("\n");
-      handleSketchDownload(TOKEN, FW_TITLE, CURRENT_VERSION);
+      handleSketchDownload();
 
     } else {
       if (msg == 0) {
@@ -315,26 +337,37 @@ void loop() {
   int irSensorValue = digitalRead(IR_PIN);
   int mq11SensorValue = analogRead(MQ11_PIN);
 
-  // Check if flame is detected
-  if (irSensorValue == LOW || mq11SensorValue >= SMOKE_THRESHOLD) {
-    if (irSensorValue == LOW) {
-      Serial.println("Flame detected!");
-      tb.sendTelemetryString(FIRE_KEY, "Flame detected!");
-    } else {
-      Serial.println("Smoke detected!");
-      tb.sendTelemetryString(SMOKE_KEY, "Smoke detected!");
-    }
+  tb.sendTelemetryData(RPC_SMOKE_SENSOR_DATA, mq11SensorValue);  //smoke sensor data
+  tb.sendTelemetryData(RPC_FIRE_SENSOR_DATA, irSensorValue);     // fire sensor data
+
+  if (FireState == 40) {
+    FireState = 0;
     activateAlarm();
-  } else {
-    deactivateAlarm();
-    tb.sendTelemetryString(FIRE_KEY, "Normal");
-    tb.sendTelemetryString(SMOKE_KEY, "Normal");
+    tb.sendAttributeBool(ACTUATOR_KEY, true);
+    Serial.println("Flame detected!");
+    tb.sendTelemetryString(FIRE_KEY, "Flame detected!");
+  }
+
+  else if (SmokeState == 40) {
+    SmokeState = 0;
+    activateAlarm();
+    tb.sendAttributeBool(ACTUATOR_KEY, true);
+    Serial.println("Smoke detected!");
+    tb.sendTelemetryString(SMOKE_KEY, "Smoke detected!");
+  }
+
+  else {
+    tb.sendTelemetryString(SMOKE_KEY, "Smoke not detected!");
+    tb.sendTelemetryString(FIRE_KEY, "Flame not detected!");
+    tb.sendAttributeBool(ACTUATOR_KEY, false);
   }
 
   // Uploads new telemetry to ThingsBoard using MQTT.
-  Serial.println("Sending fire data...");
-
-  tb.sendAttributeInt(ACTUATOR_KEY, actuatorState);
+  // Serial.println("Sending data...");
+  Serial.println("\n");
+  Serial.println(mq11SensorValue);
+  Serial.println(irSensorValue);
+  Serial.println("\n");
 
   tb.loop();
 }
